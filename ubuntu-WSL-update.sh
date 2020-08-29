f591710cd8be738e8478099abad2bfac76ab9327
MrRubberDucky99 <judahfuller@gmail.com>
2020-08-29 17:32:11 +0100

    added ubuntu update scripts

--- ubuntu-WSL-update.sh
+++ ubuntu-WSL-update.sh
@@ -1,8 +1,7 @@
-echo "Any-App setup script V1.1"
+echo "Any-App update script V1.2"
 echo "An update, upgrade and git install will be run to make sure the system is ready for install"
 sudo apt-get update -y > /dev/null 2>&1
 sudo apt-get upgrade -y > /dev/null 2>&1
-sudo apt-get -qq install git -y
 #User choses what to install
 echo "What type of install do you want:"
 echo "(1) Windows EXE (wine)" #Installs WINE
@@ -17,23 +16,22 @@ echo "(CTRL C) exit"
 read -p "Type: " type
 if [ $type -eq 1 ] #wine install
 then
-	bash "Install Scripts/wine-DU.sh"
-	echo "Wine Installed. Now exiting AnyApp."
+	bash "Install Scripts/wine-update-DU.sh"
+	echo "Wine updated. Now exiting AnyApp."
 elif [ $type -eq 4 ] #wsl setup
 then
-	bash "Install Scripts/XFCE-XRDP-DU.sh"
-	bash "Install Scripts/ZSH-P10K-DU.sh"
+	bash "Install Scripts/XFCE-XRDP-update-DU.sh"
+	bash "Install Scripts/ZSH-P10K-update-DU.sh"
 	echo "AnyApp will now exit"
 elif [ $type -eq 5 ] #part distro setup
 then
-	bash "Install Scripts/ZSH-P10K-DU.sh"
+	bash "Install Scripts/ZSH-P10K-update-DU.sh"
 	echo "AnyApp will now exit"
 elif [ $type -eq 6 ] #wsl full setup
 then
-	bash "Install Scripts/XFCE-XRDP-DU.sh"
-	bash "Install Scripts/ZSH-P10K-DU.sh"
-	bash "Install Scripts/wine-DU.sh"
-	bash "Install Scripts/background-UD.sh"
+	bash "Install Scripts/XFCE-XRDP-update-DU.sh"
+	bash "Install Scripts/ZSH-P10K-update-DU.sh"
+	bash "Install Scripts/wine-update-DU.sh"
 	echo "AnyApp will now exit"
 else
 	echo "Sorry this feature is not avaliable. Now exiting AnyApp"
--- ubuntu-update.sh
+++ ubuntu-update.sh
@@ -1,10 +1,9 @@
-echo "Any-App setup script V1.1"
+echo "Any-App update script V1.1"
 echo "An update, upgrade and git install will be run to make sure the system is ready for install"
 sudo apt-get update -y > /dev/null 2>&1
 sudo apt-get upgrade -y > /dev/null 2>&1
-sudo apt-get -qq install git -y
 #User choses what to install
-echo "What type of install do you want:"
+echo "What type of update do you want:"
 echo "(1) Windows EXE (wine)" #Installs WINE
 echo "(2) MacOS APP (darling)" #Installs Darling
 echo "(3) Both (wine, darling)" #Installs Both
@@ -17,39 +16,37 @@ echo "(CTRL C) exit"
 read -p "Type: " type
 if [ $type -eq 1 ] #wine install
 then
-	bash "Install Scripts/wine-DU.sh"
-	echo "Wine Installed. Now exiting AnyApp."
+	bash "Install Scripts/wine-update-DU.sh"
+	echo "Wine updated. Now exiting AnyApp."
 elif [ $type -eq 2 ] #darling Install
 then
-	bash "Install Scripts/darling-U.sh"
+	bash "Install Scripts/darling-update-UD.sh"
 	echo "AnyApp will now exit"
 elif [ $type -eq 3 ] #wine & darling install
 then
-	bash "Install Scripts/wine-DU.sh"
-	bash "Install Scripts/darling-U.sh"
+	bash "Install Scripts/wine-update-DU.sh"
+	bash "Install Scripts/darling-update-U.sh"
 	echo "AnyApp will now exit"
 elif [ $type -eq 4 ] #wsl setup
 then
-	bash "Install Scripts/XFCE-XRDP-DU.sh"
-	bash "Install Scripts/ZSH-P10K-DU.sh"
+	bash "Install Scripts/XFCE-XRDP-update-DU.sh"
+	bash "Install Scripts/ZSH-P10K-update-DU.sh"
 	echo "AnyApp will now exit"
 elif [ $type -eq 5 ] #part distro setup
 then
-	bash "Install Scripts/ZSH-P10K-DU.sh"
+	bash "Install Scripts/ZSH-P10K-update-DU.sh"
 	echo "AnyApp will now exit"
 elif [ $type -eq 6 ] #wsl full setup
 then
-	bash "Install Scripts/XFCE-XRDP-DU.sh"
-	bash "Install Scripts/ZSH-P10K-DU.sh"
-	bash "Install Scripts/wine-DU.sh"
-	bash "Install Scripts/background-UD.sh"
+	bash "Install Scripts/XFCE-XRDP-update-DU.sh"
+	bash "Install Scripts/ZSH-P10K-update-DU.sh"
+	bash "Install Scripts/wine-update-DU.sh"
 	echo "AnyApp will now exit"
 elif [ $type -eq 7 ] #full distro setup
 then
-	bash "Install Scripts/ZSH-P10K-DU.sh"
-	bash "Install Scripts/wine-DU.sh"
-	bash "Install Scripts/darling-U.sh"
-	bash "Install Scripts/background-UD.sh"
+	bash "Install Scripts/ZSH-P10K-update-DU.sh"
+	bash "Install Scripts/wine-update-DU.sh"
+	bash "Install Scripts/darling-update-U.sh"
 	echo "AnyApp will now exit"
 else
 	echo "Sorry this feature is not avaliable. Now exiting AnyApp"
