cdc4291dd921fa7dda15ac30f31e9aff17dfce9d
MrRubberDucky99 <judahfuller@gmail.com>
2020-08-29 10:00:21 +0100

    Added Partial Distro

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -9,6 +9,7 @@ echo "(1) Windows EXE (wine)" #Installs WINE
 echo "(x) MacOS APP (darling)" #Installs Darling
 echo "(x) Both (wine, darling)" #Installs Both
 echo "(4) Windows For Linux ubuntu setup (XFCE4, XRDP, ZSH, P10K)"
+echo "(5) Partial Distro setup"
 echo "(x) Full distro setup (for fresh ubuntu install only)" #Installs both and sets up background, ZSH, P10K, Nerd Fonts
 echo "(x) Windows For Linux Distro Setup (for fresh ubuntu wsl install only)" #Installs both and sets up background, ZSH, P10K, Nerd Fonts, XRDP, and GUI
 echo ""
@@ -65,7 +66,34 @@ then
 	echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
 	echo "P10k Installed"
 	echo "You will need to logout and back in for P10k and zsh to take effect"
-	echo "AnyApp will noe exit"
+	echo "AnyApp will now exit"
+elif [ $type -eq 5 ] #part distro setup
+then
+	echo "Installing ZSH"
+	sudo apt-get -qq install zsh -y
+	echo "50%"
+	sudo chsh -s /bin/zsh
+	chsh -s /bin/zsh
+	echo "100%"
+	echo "Installing  fonts for P10K"
+	mkdir -p ~/.local/share/fonts
+	echo "20%"
+	cd ~/.local/share/fonts
+	echo "40%"
+	wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
+	echo "60%"
+	unzip hack.zip
+	echo "80%"
+	cd
+	echo "100%"
+	echo "You will need to change your font settings in terminal to Hack Nerd Font for P10K to work best"
+	echo "Installing P10k"
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+	echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+	echo "P10k Installed"
+	echo "You will need to logout and back in for P10k and zsh to take effect"
+	echo "AnyApp will now exit"
+
 else
 	echo "Sorry this feature is under development. Now exiting AnyApp"
 fi
